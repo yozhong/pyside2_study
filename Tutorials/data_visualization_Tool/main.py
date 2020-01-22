bb388be8fb290f23d48f45622947a966b2e0d532
Luke Liao <yozhong@gmail.com>
2020-01-22 17:12:00 +0800

    Reading data from a CSV

---
+++ Tutorials/data_visualization_Tool/main.py
@@ -0,0 +1,14 @@
+import argparse
+import pandas as pd
+
+
+def read_data(fname):
+    return pd.read_csv(fname)
+
+
+if __name__ == "__main__":
+    options = argparse.ArgumentParser()
+    options.add_argument("-f", "--file", type=str, required=True)
+    args = options.parse_args()
+    data = read_data(args.file)
+    print(data)
