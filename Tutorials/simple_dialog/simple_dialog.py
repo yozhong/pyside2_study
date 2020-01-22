90e0b08a7974db6a7b4b7a92b1f10095e58d0eec
yozhong <yozhong@gmail.com>
2020-01-22 11:58:58 +0800

    Create the widgets

--- Tutorials/simple_dialog/simple_dialog.py
+++ Tutorials/simple_dialog/simple_dialog.py
@@ -1,6 +1,6 @@
 import sys
 
-from PySide2.QtWidgets import QApplication, QDialog
+from PySide2.QtWidgets import QApplication, QDialog, QLineEdit, QPushButton
 
 
 class Form(QDialog):
@@ -8,6 +8,9 @@ class Form(QDialog):
     def __init__(self, parent=None):
         super(Form, self).__init__(parent)
         self.setWindowTitle("My Form")
+        # Create widgets
+        self.edit = QLineEdit("Write my name here..")
+        self.button = QPushButton("Show Greetings")
 
 
 if __name__ == '__main__':
