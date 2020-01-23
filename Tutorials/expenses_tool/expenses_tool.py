6a7438bf139acccc106c05d9fbd95aad12d9c666
yozhong <yozhong@gmail.com>
2020-01-23 12:08:01 +0800

    Create empty window

---
+++ Tutorials/expenses_tool/expenses_tool.py
@@ -0,0 +1,21 @@
+import sys
+
+from PySide2.QtWidgets import QApplication, QMainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.setWindowTitle("Tutorial")
+
+
+if __name__ == "__main__":
+    # Qt Application
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.resize(800, 600)
+    window.show()
+
+    # Execute application
+    sys.exit(app.exec_())
