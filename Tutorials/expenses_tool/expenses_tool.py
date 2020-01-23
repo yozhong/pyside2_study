fc77fa4da992e021a1ce6906a42dce52ba57f5ac
yozhong <yozhong@gmail.com>
2020-01-23 12:27:20 +0800

    Create menu bar

--- Tutorials/expenses_tool/expenses_tool.py
+++ Tutorials/expenses_tool/expenses_tool.py
@@ -1,12 +1,28 @@
 import sys
 
-from PySide2.QtWidgets import QApplication, QMainWindow
+from PySide2.QtCore import Slot
+from PySide2.QtWidgets import QApplication, QMainWindow, QAction
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         self.setWindowTitle("Tutorial")
+
+        # Menu
+        self.menu = self.menuBar()
+        self.file_menu = self.menu.addMenu("File")
+
+        # Exit QAction
+        exit_action = QAction("Exit", self)
+        exit_action.setShortcut("Ctrl+Q")
+        exit_action.triggered.connect(self.exit_app)
+
+        self.file_menu.addAction(exit_action)
+
+    @Slot()
+    def exit_app(self, checked):
+        QApplication.quit()
 
 
 if __name__ == "__main__":
