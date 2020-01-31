3705b92da15d684c3574f1d60868f3ac833bd682
yozhong <yozhong@gmail.com>
2020-01-31 17:01:08 +0800

    Add push button widget

--- Tutorials/delftstack/basic_window.py
+++ Tutorials/delftstack/basic_window.py
@@ -1,38 +1,23 @@
 import sys
 
-from PySide2 import QtWidgets, QtGui, QtCore
+from PySide2 import QtWidgets
 
 
 def basicWindow():
     app = QtWidgets.QApplication(sys.argv)
     windowExample = QtWidgets.QWidget()
 
-    labelLeft = QtWidgets.QLabel(windowExample)
-    labelRight = QtWidgets.QLabel(windowExample)
-    labelCenter = QtWidgets.QLabel(windowExample)
+    buttonA = QtWidgets.QPushButton(windowExample)
+    labelA = QtWidgets.QLabel(windowExample)
 
-    labelLeft.setText('Left Align')
-    labelRight.setText('Right Align')
-    labelCenter.setText('Center Align')
+    buttonA.setText('Click!')
+    labelA.setText('Show Label')
 
-    windowExample.setWindowTitle('Label Align Example')
+    windowExample.setWindowTitle('Push Button Example')
     windowExample.setGeometry(100, 100, 300, 200)
 
-    labelLeft.setFixedWidth(160)
-    labelRight.setFixedWidth(160)
-    labelCenter.setFixedWidth(160)
-
-    labelLeft.setStyleSheet("border-radius: 25px;border: 1px solid red;")
-    labelRight.setStyleSheet("border-radius: 25px;border: 1px solid green;")
-    labelCenter.setStyleSheet("border-radius: 25px;border: 1px solid blue;")
-
-    labelLeft.setAlignment(QtCore.Qt.AlignLeft)
-    labelRight.setAlignment(QtCore.Qt.AlignRight)
-    labelCenter.setAlignment(QtCore.Qt.AlignCenter)
-
-    labelLeft.move(80, 40)
-    labelRight.move(80, 80)
-    labelCenter.move(80, 120)
+    buttonA.move(100, 50)
+    labelA.move(110, 100)
 
     windowExample.show()
     sys.exit(app.exec_())
