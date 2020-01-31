60cfa37991e001cf71ca03eb41ab4891be4e69b7
yozhong <yozhong@gmail.com>
2020-01-31 17:25:26 +0800

    Handle button click event

--- Tutorials/delftstack/basic_window.py
+++ Tutorials/delftstack/basic_window.py
@@ -12,6 +12,7 @@ class basicWindow(QtWidgets.QMainWindow):
         self.labelA = QtWidgets.QLabel(self)
 
         self.buttonA.setStyleSheet("background-color: red;font-size:18px;font-family:Times New Roman;")
+        self.buttonA.clicked.connect(self.clickCallback)
 
         self.buttonA.setText('Click!')
         self.labelA.setText('Show Label')
@@ -22,6 +23,9 @@ class basicWindow(QtWidgets.QMainWindow):
         self.labelA.move(110, 100)
 
         self.setGeometry(100, 100, 300, 200)
+
+    def clickCallback(self):
+        self.labelA.setText("Button is clicked")
 
 
 if __name__ == "__main__":
