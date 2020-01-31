3f99fadec9f05094e3a459a44887d718d8e561d8
yozhong <yozhong@gmail.com>
2020-01-31 18:30:16 +0800

    Grid layout stretch

--- Tutorials/delftstack/basic_window.py
+++ Tutorials/delftstack/basic_window.py
@@ -10,16 +10,11 @@ class basicWindow(QWidget):
         grid_layout = QGridLayout()
         self.setLayout(grid_layout)
 
-        button = QPushButton('1-3')
-        grid_layout.addWidget(button, 0, 0, 1, 3)
-
-        button = QPushButton('4, 7')
-        grid_layout.addWidget(button, 1, 0, -1, 1)
-
-        for x in range(1, 3):
-            for y in range(1, 3):
+        for x in range(3):
+            for y in range(3):
                 button = QPushButton(str(3 * x + y))
                 grid_layout.addWidget(button, x, y)
+            grid_layout.setColumnStretch(x, x + 1)
 
         self.setWindowTitle('Basic Grid Layout')
 
