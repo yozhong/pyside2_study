6db5f017def47827dddd2de916d7046671e796b5
yozhong <yozhong@gmail.com>
2020-01-31 16:19:21 +0800

    Change window size

--- Tutorials/delftstack/basic_window.py
+++ Tutorials/delftstack/basic_window.py
@@ -6,6 +6,7 @@ from PySide2 import QtWidgets
 def basicWindow():
     app = QtWidgets.QApplication(sys.argv)
     windowExample = QtWidgets.QWidget()
+    windowExample.setGeometry(0, 0, 400, 400)
     windowExample.setWindowTitle('Basic Window Example')
     windowExample.show()
     sys.exit(app.exec_())
