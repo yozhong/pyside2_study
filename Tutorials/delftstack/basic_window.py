f81d1763d0d97814d4538a2b4ae57ffa0296a262
yozhong <yozhong@gmail.com>
2020-02-01 21:01:06 +0800

    Use button group

--- Tutorials/delftstack/basic_window.py
+++ Tutorials/delftstack/basic_window.py
@@ -1,6 +1,6 @@
 import sys
 
-from PySide2.QtWidgets import (QApplication, QLabel, QWidget, QRadioButton, QVBoxLayout)
+from PySide2.QtWidgets import (QApplication, QLabel, QWidget, QRadioButton, QVBoxLayout, QButtonGroup)
 
 
 class basicWindow(QWidget):
@@ -12,14 +12,35 @@ class basicWindow(QWidget):
         self.rbtn2 = QRadioButton('Houston')
         self.label2 = QLabel("")
 
+        self.label3 = QLabel('Which state do you live in?')
+        self.rbtn3 = QRadioButton('New York')
+        self.rbtn4 = QRadioButton('Texas')
+        self.label4 = QLabel("")
+
+        self.btngroup1 = QButtonGroup()
+        self.btngroup2 = QButtonGroup()
+
+        self.btngroup1.addButton(self.rbtn1)
+        self.btngroup1.addButton(self.rbtn2)
+        self.btngroup2.addButton(self.rbtn3)
+        self.btngroup2.addButton(self.rbtn4)
+
         self.rbtn1.toggled.connect(self.onClicked)
         self.rbtn2.toggled.connect(self.onClicked)
+
+        self.rbtn3.toggled.connect(self.onClickedState)
+        self.rbtn4.toggled.connect(self.onClickedState)
 
         layout = QVBoxLayout()
         layout.addWidget(self.label)
         layout.addWidget(self.rbtn1)
         layout.addWidget(self.rbtn2)
         layout.addWidget(self.label2)
+
+        layout.addWidget(self.label3)
+        layout.addWidget(self.rbtn3)
+        layout.addWidget(self.rbtn4)
+        layout.addWidget(self.label4)
 
         self.setGeometry(200, 200, 300, 150)
 
@@ -32,6 +53,11 @@ class basicWindow(QWidget):
         radioBtn = self.sender()
         if radioBtn.isChecked():
             self.label2.setText("You live in " + radioBtn.text())
+
+    def onClickedState(self):
+        radioBtn = self.sender()
+        if radioBtn.isChecked():
+            self.label4.setText("You live in " + radioBtn.text())
 
 
 if __name__ == "__main__":
