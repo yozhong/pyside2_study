eb0204d75b95af0b33a605d48ab068ef250e1a29
Luke Liao <yozhong@gmail.com>
2020-02-01 12:23:46 +0800

    Checkable menu item

--- Tutorials/delftstack/basic_window.py
+++ Tutorials/delftstack/basic_window.py
@@ -1,13 +1,22 @@
 import sys
 
-from PySide2.QtGui import QIcon
-from PySide2.QtWidgets import (QApplication, QAction, qApp, QMainWindow)
+from PySide2.QtGui import QIcon, Qt
+from PySide2.QtWidgets import (QApplication, QAction, qApp, QMainWindow, QLabel)
 
 
 class basicWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
+
+        self.label = QLabel("The toggle state is ")
+        self.label.setAlignment(Qt.AlignCenter)
+
+        self.setCentralWidget(self.label)
+
+        toggleAction = QAction('&Toggle Label', self, checkable=True)
+        toggleAction.setStatusTip('Toggle the label')
+        toggleAction.triggered.connect(self.toggleLabel)
 
         exitAction = QAction(QIcon('exit.png'),
                              '&Exit', self)
@@ -19,11 +28,15 @@ class basicWindow(QMainWindow):
 
         menubar = self.menuBar()
         fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(toggleAction)
         fileMenu.addAction(exitAction)
 
         self.setGeometry(200, 200, 300, 200)
 
         self.setWindowTitle('Menu Bar Example')
+
+    def toggleLabel(self, state):
+        self.label.setText("The toggle state is {}".format(state))
 
 
 if __name__ == "__main__":
