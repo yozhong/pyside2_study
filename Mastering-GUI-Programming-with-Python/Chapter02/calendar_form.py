615e6294cd24e9a297c50c4257e7fd3b70d7ae9e
yozhong <yozhong@gmail.com>
2020-02-11 16:31:17 +0800

    [Chapter02] Building the layout

--- Mastering-GUI-Programming-with-Python/Chapter02/calendar_form.py
+++ Mastering-GUI-Programming-with-Python/Chapter02/calendar_form.py
@@ -30,6 +30,30 @@ class MainWindow(qtw.QWidget):
         # disable the first category item
         self.event_category.model().item(0).setEnabled(False)
 
+        main_layout = qtw.QHBoxLayout()
+        self.setLayout(main_layout)
+        main_layout.addWidget(self.calendar)
+        self.calendar.setSizePolicy(qtw.QSizePolicy.Expanding, qtw.QSizePolicy.Expanding)
+
+        right_layout = qtw.QVBoxLayout()
+        main_layout.addLayout(right_layout)
+        right_layout.addWidget(qtw.QLabel('Event on Date'))
+        right_layout.addWidget(self.event_list)
+        self.event_list.setSizePolicy(qtw.QSizePolicy.Expanding, qtw.QSizePolicy.Expanding)
+
+        event_form = qtw.QGroupBox('Event')
+        right_layout.addWidget(event_form)
+        event_form_layout = qtw.QGridLayout()
+        event_form.setLayout(event_form_layout)
+
+        event_form_layout.addWidget(self.event_title, 1, 1, 1, 3)
+        event_form_layout.addWidget(self.event_category, 2, 1)
+        event_form_layout.addWidget(self.event_time, 2, 2,)
+        event_form_layout.addWidget(self.allday_check, 2, 3)
+        event_form_layout.addWidget(self.event_detail, 3, 1, 1, 3)
+        event_form_layout.addWidget(self.add_button, 4, 2)
+        event_form_layout.addWidget(self.del_button, 4, 3)
+
         self.show()
 
 
