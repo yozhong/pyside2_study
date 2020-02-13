426c6bfd1b4a949e254fd6cb71e14c25735bd168
yozhong <yozhong@gmail.com>
2020-02-13 17:56:06 +0800

    [Chapter04] Add a status bar

--- Mastering-GUI-Programming-with-Python/Chapter04/text_editor.py
+++ Mastering-GUI-Programming-with-Python/Chapter04/text_editor.py
@@ -10,6 +10,13 @@ class MainWindow(qtw.QMainWindow):
         super().__init__()
         self.text_edit = qtw.QTextEdit()
         self.setCentralWidget(self.text_edit)
+        self.statusBar().showMessage('Welcome to text_editor.py')
+
+        char_count = qtw.QLabel('chars: 0')
+        self.text_edit.textChanged.connect(
+            'chars: ' + str(len(self.text_edit.toPlainText()))
+        )
+        self.statusBar().addPermanentWidget(char_count)
         self.show()
 
 
