5c9af36f07801330fb2cc97b938ac3378262ce28
yozhong <yozhong@gmail.com>
2020-02-15 15:54:57 +0800

    [Chapter05] Add view class

--- Mastering-GUI-Programming-with-Python/Chapter05/model-view_code.py
+++ Mastering-GUI-Programming-with-Python/Chapter05/model-view_code.py
@@ -26,6 +26,28 @@ class Model(qtc.QObject):
             self.error.emit(error)
 
 
+class View(qtw.QWidget):
+    submitted = qtc.Signal(str, str)
+
+    def __init__(self):
+        super().__init__()
+        self.setLayout(qtw.QVBoxLayout())
+        self.filename = qtw.QLineEdit()
+        self.filecontent = qtw.QTextEdit()
+        self.savebutton = qtw.QPushButton('Save', clicked=self.submit)
+        self.layout().addWidget(self.filename)
+        self.layout().addWidget(self.filecontent)
+        self.layout().addWidget(self.savebutton)
+
+    def submit(self):
+        filename = self.filename.text()
+        filecontent = self.filecontent.toPlainText()
+        self.submitted.emit(filename, filecontent)
+
+    def showError(self, error):
+        qtw.QMessageBox.critical(None, 'Error', error)
+
+
 class MainWindow(qtw.QMainWindow):
     def __init__(self):
         """MainWindow constructor"""
