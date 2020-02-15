7aafe42e37bb1c0181887f6e59bd41abbaa104c1
yozhong <yozhong@gmail.com>
2020-02-15 17:52:40 +0800

    [Chapter05] Implement write capabilities

--- Mastering-GUI-Programming-with-Python/Chapter05/csv_editor.py
+++ Mastering-GUI-Programming-with-Python/Chapter05/csv_editor.py
@@ -33,11 +33,52 @@ class CsvTableModel(qtc.QAbstractTableModel):
             return super().headerData(section, orientation, role)
 
     def sort(self, column, order=None):
-        self.layoutAboutToBeChanged.emit() # needs to be emitted before sort
+        self.layoutAboutToBeChanged.emit()  # needs to be emitted before sort
         self._data.sort(key=lambda x: x[column])
         if order == qtc.Qt.DescendingOrder:
             self._data.reverse()
-        self.layoutChanged.emit() # needs to be emitted after sort
+        self.layoutChanged.emit()  # needs to be emitted after sort
+
+    def flags(self, index):
+        if index not in self.readonly_indexes:
+            return super().flags(index) | qtc.Qt.ItemIsEditable
+        else:
+            return super().flags(index)
+
+    def setData(self, index, value, role=None):
+        if index.isValid() and role == qtc.Qt.EditRole:
+            self._data[index.row()][index.column()] = value
+            self.dataChanged.emit(index, index, [role])
+            return True
+        else:
+            return False
+
+    def insertRows(self, position, rows, parent=None):
+        self.beginInsertRows(
+            parent or qtc.QModelIndex(),
+            position,
+            position + rows - 1
+        )
+        for i in range(rows):
+            default_row = [''] * len(self._headers)
+            self._data.insert(position, default_row)
+        self.endInsertRows()
+
+    def removeRows(self, position, rows, parent=None):
+        self.beginRemoveRows(
+            parent or qtc.QModelIndex(),
+            position,
+            position + rows - 1
+        )
+        for i in range(rows):
+            del (self._data[position])
+        self.endRemoveRows()
+
+    def save_data(self):
+        with open(self.filename, 'w', encoding='utf-8') as fh:
+            writer = csv.writer(fh)
+            writer.writerow(self._headers)
+            writer.writerows(self._data)
 
 
 class MainWindow(qtw.QMainWindow):
