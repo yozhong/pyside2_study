ac6ebc4fd36789738e22eba03f9ea18c0b8b1de4
yozhong <yozhong@gmail.com>
2020-02-15 15:45:48 +0800

    [Chapter05] Create model-view_code.py from template file

---
+++ Mastering-GUI-Programming-with-Python/Chapter05/model-view_code.py
@@ -0,0 +1,20 @@
+import sys
+
+from PySide2 import QtWidgets as qtw
+from PySide2 import QtGui as qtg
+from PySide2 import QtCore as qtc
+
+class MainWindow(qtw.QMainWindow):
+    def __init__(self):
+        """MainWindow constructor"""
+        super().__init__()
+        # Main UI code goes here
+
+        # End main UI code
+        self.show()
+
+
+if __name__ == '__main__':
+    app = qtw.QApplication(sys.argv)
+    mw = MainWindow()
+    sys.exit(app.exec_())
