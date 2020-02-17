72e2a9d481f5aa26f687c379377c16770afb3854
yozhong <yozhong@gmail.com>
2020-02-17 11:25:36 +0800

    [Chapter05] Using the model in a view

--- Mastering-GUI-Programming-with-Python/Chapter05/csv_editor.py
+++ Mastering-GUI-Programming-with-Python/Chapter05/csv_editor.py
@@ -40,10 +40,7 @@ class CsvTableModel(qtc.QAbstractTableModel):
         self.layoutChanged.emit()  # needs to be emitted after sort
 
     def flags(self, index):
-        if index not in self.readonly_indexes:
-            return super().flags(index) | qtc.Qt.ItemIsEditable
-        else:
-            return super().flags(index)
+        return super().flags(index) | qtc.Qt.ItemIsEditable
 
     def setData(self, index, value, role=None):
         if index.isValid() and role == qtc.Qt.EditRole:
@@ -85,10 +82,51 @@ class MainWindow(qtw.QMainWindow):
     def __init__(self):
         """MainWindow constructor"""
         super().__init__()
-        # Main UI code goes here
+        self.tabview = qtw.QTableView()
+        self.tabview.setSortingEnabled(True)
+        self.setCentralWidget(self.tabview)
 
-        # End main UI code
+        menu = self.menuBar()
+        file_menu = menu.addMenu('File')
+        file_menu.addAction('Open', self.select_file)
+        file_menu.addAction('Save', self.save_file)
+
+        edit_menu = menu.addMenu('Edit')
+        edit_menu.addAction('Insert Above', self.insert_above)
+        edit_menu.addAction('Insert Below', self.insert_below)
+        edit_menu.addAction('Remove Row(s)', self.remove_rows)
+
         self.show()
+
+    def select_file(self):
+        filename, _ = qtw.QFileDialog.getOpenFileName(
+            self,
+            'Select a CSV file to open...',
+            qtc.QDir.homePath(),
+            'CSV Files (*.csv) ;; All Files (*)'
+        )
+        if filename:
+            self.model = CsvTableModel(filename)
+            self.tabview.setModel(self.model)
+
+    def save_file(self):
+        if self.model:
+            self.model.save_data()
+
+    def insert_above(self):
+        selected = self.tabview.selectedIndexes()
+        row = selected[0].row() if selected else 0
+        self.model.insertRows(row, 1, None)
+
+    def insert_below(self):
+        selected = self.tabview.selectedIndexes()
+        row = selected[-1].row() if selected else self.model.rowCount(None)
+        self.model.insertRows(row + 1, 1, None)
+
+    def remove_rows(self):
+        selected = self.tabview.selectedIndexes()
+        if selected:
+            self.model.removeRows(selected[0].row(), len(selected), None)
 
 
 if __name__ == '__main__':
