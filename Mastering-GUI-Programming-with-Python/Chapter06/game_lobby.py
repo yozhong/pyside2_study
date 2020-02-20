2fa3a9d988532e46a794ed843b3bebcdb87c556f
Luke Liao <yozhong@gmail.com>
2020-02-20 18:19:19 +0800

    [Chapter06] Add widgets

--- Mastering-GUI-Programming-with-Python/Chapter06/game_lobby.py
+++ Mastering-GUI-Programming-with-Python/Chapter06/game_lobby.py
@@ -8,9 +8,33 @@ class MainWindow(qtw.QMainWindow):
     def __init__(self):
         """MainWindow constructor"""
         super().__init__()
-        # Main UI code goes here
+        self.setWindowTitle('Fight Fighter Game Lobby')
+        cx_form = qtw.QWidget()
+        self.setCentralWidget(cx_form)
+        cx_form.setLayout(qtw.QFormLayout())
+        heading = qtw.QLabel('Fight Fighter!')
+        cx_form.layout().addRow(heading)
 
-        # End main UI code
+        inputs = {
+            'Server': qtw.QLineEdit(),
+            'Name': qtw.QLineEdit(),
+            'Password': qtw.QLineEdit(echoMode=qtw.QLineEdit.Password),
+            'Team': qtw.QComboBox(),
+            'Ready': qtw.QCheckBox('Check when ready')
+        }
+        teams = ('Crimson Shark', 'Shadow Hawks', 'Night Terrors', 'Blue Crew')
+        inputs['Team'].addItems(teams)
+        for label, widget in inputs.items():
+            cx_form.layout().addRow(label, widget)
+        self.submit = qtw.QPushButton('Connect')
+        self.submit.clicked.connect(
+            lambda: qtw.QMessageBox.information(
+                None, 'Connecting', 'Prepare for Battle!'
+            )
+        )
+        self.reset = qtw.QPushButton('Cancel')
+        self.reset.clicked.connect(self.close)
+        cx_form.layout().addRow(self.submit, self.reset)
         self.show()
 
 
