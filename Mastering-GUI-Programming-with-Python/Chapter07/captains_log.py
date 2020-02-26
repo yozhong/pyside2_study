a462f999163a18692f1f018dda17d7c882abb7a1
Luke Liao <yozhong@gmail.com>
2020-02-26 14:03:26 +0800

    [Chapter07] Record and play video

--- Mastering-GUI-Programming-with-Python/Chapter07/captains_log.py
+++ Mastering-GUI-Programming-with-Python/Chapter07/captains_log.py
@@ -8,10 +8,37 @@ class MainWindow(qtw.QMainWindow):
     def __init__(self):
         """MainWindow constructor"""
         super().__init__()
-        # Main UI code goes here
+        base_widget = qtw.QWidget()
+        base_widget.setLayout(qtw.QHBoxLayout())
+        notebook = qtw.QTabWidget()
+        base_widget.layout().addWidget(notebook)
+        self.file_list = qtw.QListWidget()
+        base_widget.layout().addWidget(self.file_list)
+        self.setCentralWidget(base_widget)
 
-        # End main UI code
+        toolbar = self.addToolBar('Transport')
+        record_act = toolbar.addAction('Rec')
+        stop_act = toolbar.addAction('Stop')
+        play_act = toolbar.addAction('Play')
+        pause_act = toolbar.addAction('Pause')
+
+        self.video_dir = qtc.QDir.home()
+        if not self.video_dir.cd('captains_log'):
+            qtc.QDir.home().mkdir('captains_log')
+            self.video_dir.cd('captains_log')
+
+        self.refresh_video_list()
+
         self.show()
+
+    def refresh_video_list(self):
+        self.file_list.clear()
+        video_files = self.video_dir.entryList(
+            ['*.ogg', '*.avi', '*.mov', '*.mp4', '*.mkv'],
+            qtc.QDir.Files | qtc.QDir.Readable
+        )
+        for fn in sorted(video_files):
+            self.file_list.addItem(fn)
 
 
 if __name__ == '__main__':
