64f77dd5a798bcea70ae0bd6cb2aac01dc3e2bd7
Luke Liao <yozhong@gmail.com>
2020-02-26 14:57:47 +0800

    [Chapter07] Video test file

---
+++ Mastering-GUI-Programming-with-Python/Chapter07/video_test.py
@@ -0,0 +1,16 @@
+from PySide2.QtCore import *
+from PySide2.QtMultimedia import *
+
+app = QCoreApplication([])
+
+for camera_info in QCameraInfo.availableCameras():
+    print('Camera: ', camera_info.deviceName())
+    camera = QCamera(camera_info)
+    r = QMediaRecorder(camera)
+    print('\tAudio Codecs: ', r.supportedAudioCodecs())
+    print('\tVideo Codecs: ', r.supportedVideoCodecs())
+    print('\tAudio Sample Rates: ', r.supportedAudioSampleRates())
+    print('\tFrame Rates: ', r.supportedFrameRates())
+    print('\tResolutions: ', r.supportedResolutions())
+    print('\tContainers: ', r.supportedContainers())
+    print('\n\n')
