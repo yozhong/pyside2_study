69e3b4c94c71bf722aca2a1a4935ba4cf9f6eebe
Luke Liao <yozhong@gmail.com>
2020-02-29 11:28:09 +0800

    [Chapter08] Building a chat GUI

--- Mastering-GUI-Programming-with-Python/Chapter08/tcp_chat.py
+++ Mastering-GUI-Programming-with-Python/Chapter08/tcp_chat.py
@@ -4,13 +4,38 @@ from PySide2 import QtGui as qtg
 from PySide2 import QtCore as qtc
 
 
+class ChatWindow(qtw.QWidget):
+    submitted = qtc.Signal(str)
+
+    def __init__(self):
+        super().__init__()
+
+        self.setLayout(qtw.QGridLayout())
+        self.message_view = qtw.QTextEdit()
+        self.message_view.setReadOnly(True)
+        self.layout().addWidget(self.message_view, 1, 1, 1, 2)
+        self.message_entry = qtw.QLineEdit()
+        self.layout().addWidget(self.message_entry, 2, 1)
+        self.send_btn = qtw.QPushButton('Send')
+        self.send_btn.clicked.connect(self.send)
+        self.layout().addWidget(self.send_btn, 2, 2)
+
+    def write_message(self, username, message):
+        self.message_view.append(f'<b>{username}: </b> {message}<br>')
+
+    def send(self):
+        message = self.message_entry.text().strip()
+        if message:
+            self.submitted.emit(message)
+            self.message_entry.clear()
+
+
 class MainWindow(qtw.QMainWindow):
     def __init__(self):
         """MainWindow constructor"""
         super().__init__()
-        # Main UI code goes here
-
-        # End main UI code
+        self.cw = ChatWindow()
+        self.setCentralWidget(self.cw)
         self.show()
 
 
